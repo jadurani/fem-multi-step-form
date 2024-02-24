81b7a6ff4df45a4a88c1ad7dd0149e7232edbb51
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-02-24 18:55:29 -0500

    fix: update form personal info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,10 @@ import FormStepGroup from "@components/FormStepGroup/FormStepGroup";
 import FormSummary from "@components/FormSummary/FormSummary";
 import { FormContext, FormDispatchContext } from "@lib/formContext";
 import { initialState, multiStepFormReducer } from "@lib/formReducer";
+import Image from "next/image";
 import { useReducer } from "react";
+
+import desktopBg from "@assets/images/bg-sidebar-desktop.svg";
 
 export default function Home() {
   const [state, dispatch] = useReducer(multiStepFormReducer, initialState);
@@ -16,27 +19,37 @@ export default function Home() {
   return (
     <FormContext.Provider value={state}>
       <FormDispatchContext.Provider value={dispatch}>
-        <main className="">
-          <FormStepGroup activeStep={activeStep} />
+        <main className="h-screen flex items-center justify-center">
+          <div className="w-[940px] h-[600px] bg-white p-4 flex ">
+            <div className="relative">
+              <div className="p-8 absolute z-10">
+                <FormStepGroup activeStep={activeStep} />
+              </div>
 
-          <div hidden={activeStep != 1}>
-            <FormPersonalInfo />
-          </div>
+              <Image src={desktopBg} alt="" />
+            </div>
 
-          <div hidden={activeStep != 2}>
-            <FormSelectPlan />
-          </div>
+            <div className="py-6 w-[450px] mx-auto">
+              <div className="h-full" hidden={activeStep != 1}>
+                <FormPersonalInfo />
+              </div>
 
-          <div hidden={activeStep != 3}>
-            <FormPickAddOn
-              online={online}
-              storage={storage}
-              profile={profile}
-            />
-          </div>
+              <div className="h-full" hidden={activeStep != 2}>
+                <FormSelectPlan />
+              </div>
 
-          <div hidden={activeStep != 4}>
-            <FormSummary />
+              <div className="h-full" hidden={activeStep != 3}>
+                <FormPickAddOn
+                  online={online}
+                  storage={storage}
+                  profile={profile}
+                />
+              </div>
+
+              <div className="h-full" hidden={activeStep != 4}>
+                <FormSummary />
+              </div>
+            </div>
           </div>
         </main>
       </FormDispatchContext.Provider>
