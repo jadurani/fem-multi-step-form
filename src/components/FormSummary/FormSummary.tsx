2fadff7571c7fd0f817653748dd1a22dbc19f2f4
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-02-24 21:01:30 -0500

    refactor: showing add-ons in form summary

--- src/components/FormSummary/FormSummary.tsx
+++ src/components/FormSummary/FormSummary.tsx
@@ -1,22 +1,31 @@
 import Button from "@components/Button/Button";
 import FormFinished from "@components/FormFinished/FormFinished";
-import { ADD_ON_PRICES, DURATION_ABBV, PLAN_PRICES } from "@lib/form.constant";
+import {
+  ADD_ONS,
+  ADD_ON_PRICES,
+  DURATION_ABBV,
+  PLAN_PRICES,
+} from "@lib/form.constant";
 import { FormContext, FormDispatchContext } from "@lib/formContext";
 import { useContext, useState } from "react";
 
 const FormSummary = () => {
   const [submitted, setSubmitted] = useState(false);
-  const { duration, selectedPlan, online, storage, profile } =
-    useContext(FormContext);
+  const formData = useContext(FormContext);
+  const { duration, selectedPlan } = formData;
   const dispatch = useContext(FormDispatchContext);
 
   const durationAbbv = DURATION_ABBV[duration];
   const selectedPlanPrice = PLAN_PRICES[duration][selectedPlan];
-  const onlinePrice = online ? ADD_ON_PRICES[duration]["online"] : 0;
-  const storagePrice = storage ? ADD_ON_PRICES[duration]["storage"] : 0;
-  const profilePrice = profile ? ADD_ON_PRICES[duration]["profile"] : 0;
 
-  const total = selectedPlanPrice + onlinePrice + storagePrice + profilePrice;
+  let addOnTotal = 0;
+  ADD_ONS.forEach(({ id }) => {
+    if (formData[id]) {
+      addOnTotal += ADD_ON_PRICES[duration][id];
+    }
+  });
+
+  const total = selectedPlanPrice + addOnTotal;
 
   if (submitted) {
     return (
@@ -36,7 +45,7 @@ const FormSummary = () => {
       </div>
 
       <div className="rounded-md px-4 py-2 lg:py-4 lg:px-8 bg-grey-veryLight text-[14px] mt-4">
-        <div className="flex justify-between items-center border-b border-grey/40 pb-4">
+        <div className={`flex justify-between items-center`}>
           <div>
             <span className="capitalize text-denim font-medium block my-2">
               <span>{selectedPlan}</span> <span>({duration})</span>
@@ -54,40 +63,30 @@ const FormSummary = () => {
           </div>
         </div>
 
-        {online && (
-          <div className="flex justify-between items-center my-2">
-            <div className="text-grey">Online service</div>
-            <div className="text-denim">
-              +${onlinePrice}/{durationAbbv}
-            </div>
-          </div>
-        )}
+        <div
+          className={`w-full ${addOnTotal ? "border-b border-grey/40 my-4" : ""}`}></div>
 
-        {storage && (
-          <div className="flex justify-between items-center my-2">
-            <div className="text-grey">Larger storage</div>
-            <div className="text-denim">
-              +${storagePrice}/{durationAbbv}
+        {ADD_ONS.map(({ id, title }, idx) => {
+          if (!formData[id]) {
+            return <></>;
+          }
+
+          return (
+            <div className="flex justify-between items-center my-2" key={idx}>
+              <div className="text-grey">{title}</div>
+              <div className="text-denim">
+                +${ADD_ON_PRICES[duration][id]}/{durationAbbv}
+              </div>
             </div>
-          </div>
-        )}
-        {profile && (
-          <div className="flex justify-between items-center my-2">
-            <div className="text-grey">Customizable Profile</div>
-            <div className="text-denim">
-              +${profilePrice}/{durationAbbv}
-            </div>
-          </div>
-        )}
+          );
+        })}
       </div>
-
       <div className="flex justify-between items-center px-4 lg:px-8 my-4">
         <div className="text-grey text-[14px]">Total (per year)</div>
         <div className="text-purple font-bold text-body lg:text-h5">
           ${total}/{durationAbbv}
         </div>
       </div>
-
       <div className="fixed py-4 px-6 bottom-0 left-0 lg:static w-full flex justify-between mt-auto bg-white z-20">
         <Button
           style="clear"
